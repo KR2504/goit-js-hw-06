4dfab777a84f19dbc55509d9369c6138aae3d6f4
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-03-19 16:47:46 +0200

    task-9__js-module-6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,28 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+    btn: document.querySelector('.change-color'),
+    span: document.querySelector('.color'),
+    body: document.querySelector('body', )
 }
+refs.btn.addEventListener('click', getRandomHexColor);
+refs.btn.addEventListener('click', onChangeSpanText);
+
+function getRandomHexColor() {
+    return refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+      .padStart(6, 0)}`;
+}
+
+function onChangeSpanText() {
+    return refs.span.textContent = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// function getRandomHexColor() {
+//     let changeColor = refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//       .padStart(6, 0)}`;
+//     refs.span.textContent = changeColor
+
+//     return changeColor;
+// }
