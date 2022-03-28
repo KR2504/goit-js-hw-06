b52b4440d78479ca7f85d16533a96aebe45508ab
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-03-28 13:50:41 +0300

    update task-06-js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,15 @@
 const refOnInput = document.querySelector('input[data-length="6"]')
 
-refOnInput.addEventListener('blur', onInputBlur)
+refOnInput.addEventListener('blur', onInputBlur);
+
 
 function onInputBlur(event) {
     refOnInput.classList.add('invalid');
     if (event.currentTarget.value.length === Number(refOnInput.dataset.length)) {
-        refOnInput.classList.replace('invalid', 'valid');
-    }
-    // event.currentTarget.value.length !== Number(refOnInput.dataset.length) ? refOnInput.classList.add('invalid') : refOnInput.classList.replace('invalid', 'valid');
+
+        refOnInput.classList.replace('invalid', 'valid')
+    } else refOnInput.classList.replace('valid', 'invalid')
 }
+
+
+// event.currentTarget.value.length !== Number(refOnInput.dataset.length) ? refOnInput.classList.add('invalid') : refOnInput.classList.replace('invalid', 'valid');
